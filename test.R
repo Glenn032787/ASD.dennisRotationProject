da748e4b21c3c5842aabc6f67851df38ddd9cd55
Glenn <glennkengmanchang@gmail.com>
2021-12-04 23:28:06 -0800

    fixed bug in bed file

--- test.R
+++ test.R
@@ -19,8 +19,20 @@ homolog <- full_join(excel_data, mouseToHuman, by = c("Geneid" = "MGI.symbol"))
 homolog %>% 
   select(Geneid, HGNC.symbol, `logFC MAA-PBS`)
 
-saveRDS(homolog, "mouse_human_joint_data.RDS")
+homolog <- readRDS("mouse_human_joint_data.RDS")
 
-getBM(mart = human, attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position", "strand"), 
-      filters = "hgnc_symbol", value = "ALK")
+BED_file <- homolog %>%
+  select(HGNC.symbol) %>%
+  drop_na(HGNC.symbol) %>%
+  pull() %>%
+  getBM(mart = human, attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position"), 
+        filters = "hgnc_symbol", value = .) %>%
+  filter(!is.na(as.numeric(chromosome_name)) | chromosome_name %in% c("X", "Y"))
 
+BED_file.FINAL <- BED_file %>%
+  mutate(chrom = str_c("chr",chromosome_name )) %>%
+  rename("chromStart" = start_position, 
+         "chromEnd" = end_position) %>%
+  select(chrom, chromStart, chromEnd)
+
+write_delim(BED_file.FINAL, "diffExpressionRegion.bed", delim = "\t", col_names = FALSE)
